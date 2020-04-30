836de0614f0ff3952a708306168540efab9be7a9
Brook Beyene <brookbeyene37@gmail.com>
2020-04-30 23:43:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,20 @@ def unsing_push(array, string)
 end
 
 def using_unshift(array, string)
-  array.unshift()
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_arg(array)
+  
